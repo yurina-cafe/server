04cdcd630ea604cf752ec66303b71c233a1811b1
chenmh0901 <1210204146@zust.edu.cn>
2024-06-07 09:28:55 -0400

    update DebugArticle

--- locales/zh-CN.ts
+++ locales/zh-CN.ts
@@ -1,5 +1,8 @@
+
 export const debugPrefix = {
   PostArticleSuccess: "文章发布成功",
+  DeleteArticleSuccess: "文章删除成功",
+  GetArticleSuccess: "文章获取成功",
   Common: {
     Prefix: "[Article]",
     Spacing: " ",
